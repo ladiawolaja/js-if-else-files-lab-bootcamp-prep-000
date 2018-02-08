7789d36ad6fdc333687fb8dcdfa7100e1626187a
Samson Awolaja <samson.awolaja@gmail.com>
2018-02-08 21:24:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+<<<<<<< HEAD
 var name = "Susan"
 var height = 70
+=======
+var name = "Joe"
+var height = "74"
+if (name === "Susan") {
+  console.log("The name is correct")
+} else {
+  console.log("The name is incorrect")
+}
+>>>>>>> 58df8649a46b1b8fbc827db8844404f12ca3e8f3
 
 // Don't worry about this
 module.exports = { name, height
